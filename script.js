31461af78f80b1873be6bd61d8d18d7ff171bba9
rezkycx <rezky_cx@hotmail.com>
2024-09-12 20:23:01 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    return a+b;
+}
+function substract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    if (b === 0){
+        throw new Error("Division by Zero");
+    }
+    return a/b;
+}
+
+function isNameValid(name) {
+    return /^[a-zA-Z]+$/.test(name);
+
+}
+
+function getAPI(url,callback) {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error("Error fetching data: " + error))
+}
